8d0b34d36edbe5ddba183147412b911a2c0df624
Hoshiyuki-Api <websitehoshiyuki@gmail.com>
2024-06-25 21:23:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restx import Api
 from routes.index import index_app
 # Routes Downloader
 from routes.downloader import tiktok_bp, igdl_bp, twitter_bp, tiktokdlrek as tiktok_ns, instagramdlrek as igdl_ns, twitterdlrek as twitter_ns
-from routes.downloader import facebook_bp, mediafire_bp, pinterestvid_bp, laheludl_bp, facebookdlrek as fbdl_ns, mediafiredlrek as mdf_ns, pinterestviddlrek as pinvid_ns, laheludlrek as lahelu_ns
+from routes.downloader import facebook_bp, mediafire_bp, pinterestvid_bp, laheludl_bp, ytdl_bp, facebookdlrek as fbdl_ns, mediafiredlrek as mdf_ns, pinterestviddlrek as pinvid_ns, laheludlrek as lahelu_ns,  ytdlrek as ytdl_ns
 from routes.tools import igstalk_bp, stalkigrek as stalkig_ns
 from routes.useragent import useragent_bp, api as useragent_ns
 from routes.dash_check import check_bp, api as check_ns
@@ -24,6 +24,7 @@ app.register_blueprint(facebook_bp, url_prefix='/api/fbdl')
 app.register_blueprint(mediafire_bp, url_path='/api/mediafire')
 app.register_blueprint(pinterestvid_bp, url_path='/api/pinvid')
 app.register_blueprint(laheludl_bp, url_prefix='/api/laheludl')
+app.register_blueprint(ytdl_bp, url_prefix='/api/ytdl')
 app.register_blueprint(igstalk_bp, url_prefix='/api/stalkig')
 # Register other blueprints like twitter_bp, facebook_bp, etc., if available
 
@@ -43,6 +44,7 @@ api.add_namespace(fbdl_ns, path='/api/fbdl')      # NameSpace Untuk Facebook
 api.add_namespace(mdf_ns, path='/api/mediafire')  # NameSpace Untuk Mediafire
 api.add_namespace(pinvid_ns, path='/api/pinvid')  # NameSpace Untuk Pinterest Video
 api.add_namespace(lahelu_ns, path='/api/lahelu')  # NameSpace Untuk Lahelu
+api.add_namespace(ytdl_ns, path='/api/ytdl')
 # NameSpace Tools
 api.add_namespace(stalkig_ns, path='/api/stalkig')
 
