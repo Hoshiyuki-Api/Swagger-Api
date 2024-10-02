da90b950108dfca062bf84c31d9b5514ac5769a8
XENZI GANZZ <82303963+Xenzi-XN1@users.noreply.github.com>
2024-10-02 13:45:13 +0700

    Add files via upload

--- routes/ai.py
+++ routes/ai.py
@@ -6,7 +6,8 @@ from datetime import datetime
 from flask import Blueprint, jsonify, request, redirect
 from flask_restx import Namespace, Resource
 
-hercai_bp = Blueprint('_openai-hercai', __name__)
+aivoice_bp = Blueprint('aivoice', __name__)
+hercai_bp = Blueprint('hercai', __name__)
 blackbox_bp = Blueprint('_openai-blackbox', __name__)
 deepai_bp = Blueprint('_openai-deepai', __name__)
 simi_bp = Blueprint('_openai_simi', __name__)
@@ -97,6 +98,7 @@ def check_and_update_request_limit(apikey):
 
 
 # Namespace untuk Flask-RESTX
+aivoicerek = Namespace('ai', description='AI Api')
 hercairek = Namespace('ai', description='AI Api')
 blackboxrek = Namespace('ai', description='AI Api')
 deepairek = Namespace('ai', description='AI Api')
@@ -106,6 +108,100 @@ texttirek = Namespace('ai', description='AI Api')
 animediff = Namespace('ai', description='AI Api')
 bingimg = Namespace('ai', description='AI Api')
 imgtotext = Namespace('ai', description='AI Api')
+
+@aivoicerek.route('')
+class DownloadaivoiceResource(Resource):
+    @aivoicerek.doc(params={
+        'text': 'Input Text',
+        'type': 'input Type Voice'
+        'apikey': 'API Key for authentication'
+    })
+    def get(self):
+        """
+        Ai Generate Voice Api.
+
+        Parameters:
+        - text: Text (required)
+        - type: Type Anime
+        - apikey: API Key for authentication (required)
+        """
+        text = request.args.get('text')
+        type = request.args.get('type')
+        apikey = request.args.get('apikey')
+
+        if not text:
+            return jsonify({"creator": "AmmarBN", "error": "Parameter 'text' diperlukan."})
+        if not type:
+            return jsonify({"creator": "AmmarBN", "error": "Parameter 'type' diperlukan."})
+        if not apikey:
+            return jsonify({"creator": "AmmarBN", "error": "Parameter 'apikey' diperlukan."})
+
+        limit_error = check_and_update_request_limit(apikey)
+        if limit_error:
+            return jsonify(limit_error[0]), limit_error[1]
+
+        try:
+            generate_random_ips = f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+            ins_agents = [
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.1.2 Safari/602.3.12",
+                "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Mobile Safari/537.36"
+            ]
+            random_ins_agent = random.choice(ins_agents)
+            models = {
+                "miku": {"voice_id": "67aee909-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Hatsune Miku"},
+                "nahida": {"voice_id": "67ae0979-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Nahida (Exclusive)"},
+                "nami": {"voice_id": "67ad95a0-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Nami"},
+                "ana": {"voice_id": "f2ec72cc-110c-11ef-811c-00163e0255ec", "voice_name": "Ana(Female)"},
+                "optimus_prime": {"voice_id": "67ae0f40-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Optimus Prime"},
+                "goku": {"voice_id": "67aed50c-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Goku"},
+                "taylor_swift": {"voice_id": "67ae4751-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Taylor Swift"},
+                "elon_musk": {"voice_id": "67ada61f-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Elon Musk"},
+                "mickey_mouse": {"voice_id": "67ae7d37-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Mickey Mouse"},
+                "kendrick_lamar": {"voice_id": "67add638-5d4b-11ee-a861-00163e2ac61b", "voice_name": "Kendrick Lamar"},
+                "angela_adkinsh": {"voice_id": "d23f2adb-5d1b-11ee-a861-00163e2ac61b", "voice_name": "Angela Adkinsh"},
+                "eminem": {"voice_id": "c82964b9-d093-11ee-bfb7-e86f38d7ec1a", "voice_name": "Eminem"}
+            }
+            list_name = list(models.keys())
+            if type in list_name:
+                voice_id = models[type]['voice_id']
+                ngeloot = {
+                    "raw_text": text,
+                    "url": "https://filme.imyfone.com/text-to-speech/anime-text-to-speech/",
+                    "product_id": "200054",
+                    "convert_data": [
+                        {
+                            "voice_id": voice_id,
+                            "speed": "1",  # maksimal 100 wak normal 1
+                            "volume": "50",  # maksimal 100 normal 50
+                            "text": text,
+                            "pos": 0
+                        }
+                    ]
+                }
+                
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    'X-Forwarded-For': generate_random_ips,
+                    'User-Agent': random_ins_agent
+                }
+
+                response = requests.post('https://voxbox-tts-api.imyfone.com/pc/v1/voice/tts', json=ngeloot, headers=headers)
+                response.raise_for_status()  # Menghasilkan exception untuk kode status HTTP yang tidak sukses
+                data = response.json()
+                convert_result = data.get("data", {}).get("convert_result", [{}])[0]
+                return jsonify({
+                    'creator': 'AmmarBN',
+                    'modelName': type,
+                    'voiceId': voice_id,
+                    'url_voice': convert_result.get("oss_url"),
+                    'status': True
+                })
+            else:return jsonify({"creator": "AmmarBN", "list_model": list(models.keys()) , "error": 'model tidak di temukan'}), 500
+        except Exception as e:
+            return jsonify({"creator": "AmmarBN", "error": str(e)}), 500
+        
 
 @hercairek.route('')
 class DownloadhercaiResource(Resource):
