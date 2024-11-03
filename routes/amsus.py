5125155e768619c3403f3527ac1c25fec926c2d6
XENZI GANZZ <82303963+Xenzi-XN1@users.noreply.github.com>
2024-11-03 18:04:54 +0700

    Update amsus.py

--- routes/amsus.py
+++ routes/amsus.py
@@ -138,6 +138,6 @@ def checknik():
         if jenis in "L":type = "Pria"
         elif jenis in "P":type = "Perempuan"
         else:type = None
-        return jsonify({"creator": "AmmarBN", "result":{json.loads(resp)}} #"nama": nama, "tgllahir": lahir, "nomorhp": phone, "umur": umur, "jeniskelamin": type})
+        return jsonify({"creator": "AmmarBN", "result":{json.loads(resp)}}) #"nama": nama, "tgllahir": lahir, "nomorhp": phone, "umur": umur, "jeniskelamin": type})
     except requests.exceptions.RequestException as e:return jsonify({'error': str(e)})
 
