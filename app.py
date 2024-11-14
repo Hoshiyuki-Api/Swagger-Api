2279906447f0c038b4cfb5fcdd0852f0fbc5748b
XENZI GANZZ <82303963+Xenzi-XN1@users.noreply.github.com>
2024-11-14 09:30:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from routes.dash_regis import regis_app
 from routes.dashboard import dashboard_bp
 # Routes Downloader
 from routes.downloader import tiktok_bp, igdl_bp, twitter_bp, tiktokdlrek as tiktok_ns, instagramdlrek as igdl_ns, twitterdlrek as twitter_ns
-from routes.downloader import facebook_bp, mediafire_bp, pinterestvid_bp, laheludl_bp, ytdl_bp, facebookdlrek as fbdl_ns, mediafiredlrek as mdf_ns, pinterestviddlrek as pinvid_ns, laheludlrek as lahelu_ns,  ytdlrek as ytdl_ns
+from routes.downloader import facebook_bp, mediafire_bp, pinterestvid_bp, laheludl_bp, ytdlmp3_bp, ytdlmp4_bp, facebookdlrek as fbdl_ns, mediafiredlrek as mdf_ns, pinterestviddlrek as pinvid_ns, laheludlrek as lahelu_ns,  ytdlmp4rek as ytdl4_ns,  ytdlmp3rek as ytdl3_ns
 # Routes Tools
 from routes.tools import igstalk_bp, remove_bp, cuaca_bp, ffstalk_bp, removebg2_bp, ssweb_bp, stalkigrek as stalkig_ns, removebgrek as removebg_ns, cuacarek as cuaca_ns, ffstalkgrek as ffstalk_ns, removebg2grek as remove2_ns, sswebgrek as ssweb_ns
 # Routes api
@@ -60,7 +60,8 @@ app.register_blueprint(facebook_bp, url_prefix='/api/fbdl')
 app.register_blueprint(mediafire_bp, url_path='/api/mediafire')
 app.register_blueprint(pinterestvid_bp, url_path='/api/pinvid')
 app.register_blueprint(laheludl_bp, url_prefix='/api/laheludl')
-app.register_blueprint(ytdl_bp, url_prefix='/api/ytdl')
+app.register_blueprint(ytdlmp4_bp, url_prefix='/api/ytmp4')
+app.register_blueprint(ytdlmp3_bp, url_prefix='/api/ytmp3')
 # Register tools
 app.register_blueprint(igstalk_bp, url_prefix='/api/stalkig')
 app.register_blueprint(remove_bp, url_prefix='/api/removebg')
@@ -96,7 +97,8 @@ api.add_namespace(fbdl_ns, path='/api/fbdl')      # NameSpace Untuk Facebook
 api.add_namespace(mdf_ns, path='/api/mediafire')  # NameSpace Untuk Mediafire
 api.add_namespace(pinvid_ns, path='/api/pinvid')  # NameSpace Untuk Pinterest Video
 api.add_namespace(lahelu_ns, path='/api/lahelu')  # NameSpace Untuk Lahelu
-api.add_namespace(ytdl_ns, path='/api/ytdl')
+api.add_namespace(ytdl4_ns, path='/api/mp4')
+api.add_namespace(ytdl3_ns, path='/api/mp3')
 # NameSpace Tools
 api.add_namespace(stalkig_ns, path='/api/stalkig')
 api.add_namespace(removebg_ns, path='/api/removebg')
