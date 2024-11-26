b1993b27d80bac0458a1f5071a674baa9d075d5e
XENZI GANZZ <82303963+Xenzi-XN1@users.noreply.github.com>
2024-11-26 20:38:45 +0700

    Add files via upload

--- routes/downloader.py
+++ routes/downloader.py
@@ -184,10 +184,10 @@ class DownloadigResource(Resource):
         inner_html_match = re.search(r'innerHTML\s*=\s*"(.*?)";', html_content, re.DOTALL)
         if inner_html_match:
             try:
-           	extracted_html = inner_html_match.group(1)
-           	thumb_url = re.search('"https://cdn.instasave.website/(.*?)"', extracted_html)
-           	link_vido = thumb_url.group(1)
-           	urls = "https://cdn.instasave.website/{}".format(link_vido.replace("\\", ""))
+               extracted_html = inner_html_match.group(1)
+               thumb_url = re.search('"https://cdn.instasave.website/(.*?)"', extracted_html)
+               link_vido = thumb_url.group(1)
+               urls = "https://cdn.instasave.website/{}".format(link_vido.replace("\\", ""))
             except:urls = None
         else:urls = None
         rse.append(extracted_html)
