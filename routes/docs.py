fa21e361a45a3bf43598933e365d341e383d3822
YukiSmall <104218064+AmmarrBN@users.noreply.github.com>
2024-12-02 18:01:00 +0700

    Update docs.py

--- routes/docs.py
+++ routes/docs.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 docs_app = Blueprint('docs', __name__, url_prefix='/')
 
-@docs_app.route('/docs')
+@docs_app.route('')
 def docs():
     # Render the HTML template located in the 'templates' directory
      return render_template('api-docs.html')
