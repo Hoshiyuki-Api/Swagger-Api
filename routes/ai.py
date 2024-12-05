28047db9be00affa3a699fbfeb8ecd47f1beb0b6
Hoshiyuki-Api <websitehoshiyuki@gmail.com>
2024-12-05 22:46:55 +0700

    Update ai.py

--- routes/ai.py
+++ routes/ai.py
@@ -736,16 +736,10 @@ class Resourceflux(Resource):
         if not query:
             return jsonify({"creator": "AmmarBN", "error": "Parameter 'text' diperlukan."})
 
-        try:
-            # Memanggil API eksternal
-            api_url = f"https://api.ryzendesu.vip/api/ai/flux-diffusion?prompt={query}"
-            response = requests.get(api_url)
-
-            if response.status_code == 200:
-                # Mengembalikan gambar langsung dari data biner
-                return Response(response.content, content_type="image/jpeg")
-            else:
-                return jsonify({"creator": "AmmarBN", "status": False, "error": f"Error from external API: {response.status_code}"})
+	try:
+		api_url = f"https://api.ryzendesu.vip/api/ai/flux-diffusion?prompt={query}"
+		response = requests.get(api_url)
+		return Response(response.content, content_type="image/jpeg")
         except Exception as e:
             return jsonify({'status': False, 'msg': f'Error: {str(e)}'})
 
