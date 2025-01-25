ddb7abb03960fa272402268c33153eb8654bc0b4
XENZI GANZZ <82303963+Xenzi-XN1@users.noreply.github.com>
2025-01-25 21:24:31 +0700

    Add files via upload

--- routes/downloader.py
+++ routes/downloader.py
@@ -713,7 +713,8 @@ class DownloadbilibiliResource(Resource):
                      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
                  }
                  array_video = []
-                 resp = requests.get("https://api.bilibili.tv/intl/gateway/web/playurl", params=params, proxies=proxy)
+                 scraper = cloudscraper.create_scraper()
+                 resp = scraper.get("https://api.bilibili.tv/intl/gateway/web/playurl", params=params)
                  return jsonify({'creator': 'AmmarBN','status': True,'result': [resp.json()]})
                  for video in resp.json()['data']['playurl']['video']:
                      array_video.append({
@@ -750,9 +751,9 @@ class DownloadxiaohongshuResource(Resource):
 
         try:
             scraper = cloudscraper.create_scraper()
-            requ  = scraper.get(url).text
-            video = re.search('"masterUrl":"(.*?)"', requ).group(1)
-            return jsonify({'creator': 'AmmarBN','status': True,'result': video.encode('utf-8').decode('unicode_escape')})
+            requ  = scraper.get(url)
+            #video = re.search('"masterUrl":"(.*?)"', requ).group(1)
+            return jsonify({'creator': 'AmmarBN','status': True,'result': [requ.text]}) #video.encode('utf-8').decode('unicode_escape')})
         except Exception as e:
             return jsonify({'status': False, 'msg': f'Error: {str(e)}'})
 
